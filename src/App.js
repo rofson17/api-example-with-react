0f14fef0ece39e9994659c17205d30fc2cff713c
Siam <rofson.siam17@gamil.com>
2020-09-30 16:30:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+//npm install axios
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [num, setNum] = useState();
+  const [name, setName] = useState();
+  const [moves, setMoves] = useState();
+
+  //useEffect need a function
+  useEffect(() => {
+    async function getData() {
+
+      //we get an api using async
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+
+      //console.log(res);
+
+      setName(res.data.name);
+      setMoves(res.data.moves.length);
+    }
+
+    getData();
+
+  });
+  let google = `https://www.google.com/search?q=${name}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="main">
+          <h1>You choose:  <span style={{ color: "red" }}> {num} </span></h1>
+          <h1>My name is  <span style={{ color: "red" }}> {name} </span></h1>
+          <h1>I have <span style={{ color: "red" }}> {moves} </span> moves</h1>
+          <h4><a href={google} target="_blank">Learn About  {name} on google</a></h4>
+
+          <select  value={num} onChange={(event) => {
+            // select the value 
+            setNum(event.target.value);
+          }}>
+            <option value='0' selected>0</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+            <option value='13'>13</option>
+            <option value='14'>14</option>
+            <option value='15'>15</option>
+            <option value='16'>16</option>
+            <option value='17'>17</option>
+            <option value='18'>18</option>
+            <option value='19'>19</option>
+            <option value='20'>20</option>
+            <option value='21'>21</option>
+            <option value='22'>22</option>
+            <option value='23'>23</option>
+            <option value='24'>24</option>
+            <option value='25'>25</option>
+
+          </select>
+        </div>
       </header>
-    </div>
-  );
-}
+    </>
+  )
+};
 
 export default App;
